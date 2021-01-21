9c05ca490a75a79e626cf6b3296722dbe46d7e01
David Lim <37480082+drlim2u@users.noreply.github.com>
2021-01-21 16:48:03 +0000

    Removed unnecessary "this" pointers

--- SingleDigitNumericDisplay.cpp
+++ SingleDigitNumericDisplay.cpp
@@ -6,89 +6,89 @@
 
 SingleDigitNumericDisplay::SingleDigitNumericDisplay(PinName pinA, PinName pinB, PinName pinC, PinName pinD,
                                                      PinName pinE, PinName pinF, PinName pinG, PinName pinDP2) {
-    this->ledA = new DigitalOut(pinA);
-    this->ledB = new DigitalOut(pinB);
-    this->ledC = new DigitalOut(pinC);
-    this->ledD = new DigitalOut(pinD);
-    this->ledE = new DigitalOut(pinE);
-    this->ledF = new DigitalOut(pinF);
-    this->ledG = new DigitalOut(pinG);
-    this->ledDP2 = new DigitalOut(pinDP2);
+    ledA = new DigitalOut(pinA);
+    ledB = new DigitalOut(pinB);
+    ledC = new DigitalOut(pinC);
+    ledD = new DigitalOut(pinD);
+    ledE = new DigitalOut(pinE);
+    ledF = new DigitalOut(pinF);
+    ledG = new DigitalOut(pinG);
+    ledDP2 = new DigitalOut(pinDP2);
 
-    this->clearDisplay();
+    clearDisplay();
 }
 
 void SingleDigitNumericDisplay::setDisplay(int number) {
-    this->clearDisplay();
+    clearDisplay();
     switch (number) {
         case 0:
-            this->ledA->write(0);
-            this->ledB->write(0);
-            this->ledC->write(0);
-            this->ledD->write(0);
-            this->ledE->write(0);
-            this->ledF->write(0);
+            ledA->write(0);
+            ledB->write(0);
+            ledC->write(0);
+            ledD->write(0);
+            ledE->write(0);
+            ledF->write(0);
             break;
         case 1:
-            this->ledB->write(0);
-            this->ledC->write(0);
+            ledB->write(0);
+            ledC->write(0);
             break;
         case 2:
-            this->ledA->write(0);
-            this->ledB->write(0);
-            this->ledD->write(0);
-            this->ledE->write(0);
-            this->ledG->write(0);
+            ledA->write(0);
+            ledB->write(0);
+            ledD->write(0);
+            ledE->write(0);
+            ledG->write(0);
             break;
         case 3:
-            this->ledA->write(0);
-            this->ledB->write(0);
-            this->ledC->write(0);
-            this->ledD->write(0);
-            this->ledG->write(0);
+            ledA->write(0);
+            ledB->write(0);
+            ledC->write(0);
+            ledD->write(0);
+            ledG->write(0);
             break;
         case 4:
-            this->ledB->write(0);
-            this->ledC->write(0);
-            this->ledF->write(0);
-            this->ledG->write(0);
+            ledB->write(0);
+            ledC->write(0);
+            ledF->write(0);
+            ledG->write(0);
             break;
         case 5:
-            this->ledA->write(0);
-            this->ledC->write(0);
-            this->ledD->write(0);
-            this->ledF->write(0);
-            this->ledG->write(0);
+            ledA->write(0);
+            ledC->write(0);
+            ledD->write(0);
+            ledF->write(0);
+            ledG->write(0);
             break;
         case 6:
-            this->ledA->write(0);
-            this->ledC->write(0);
-            this->ledD->write(0);
-            this->ledE->write(0);
-            this->ledF->write(0);
-            this->ledG->write(0);
+            ledA->write(0);
+            ledC->write(0);
+            ledD->write(0);
+            ledE->write(0);
+            ledF->write(0);
+            ledG->write(0);
             break;
         case 7:
-            this->ledA->write(0);
-            this->ledB->write(0);
-            this->ledC->write(0);
+            ledA->write(0);
+            ledB->write(0);
+            ledC->write(0);
             break;
         case 8:
-            this->ledA->write(0);
-            this->ledB->write(0);
-            this->ledC->write(0);
-            this->ledD->write(0);
-            this->ledE->write(0);
-            this->ledF->write(0);
-            this->ledG->write(0);
+            ledA->write(0);
+            ledB->write(0);
+            ledC->write(0);
+            ledD->write(0);
+            ledE->write(0);
+            ledF->write(0);
+            ledG->write(0);
             break;
         case 9:
-            this->ledA->write(0);
-            this->ledB->write(0);
-            this->ledC->write(0);
-            this->ledD->write(0);
-            this->ledF->write(0);
-            this->ledG->write(0);
+            ledA->write(0);
+            ledB->write(0);
+            ledC->write(0);
+            ledD->write(0);
+            ledF->write(0);
+            ledG->write(0);
             break;
         default:
             break;
@@ -96,12 +96,12 @@ void SingleDigitNumericDisplay::setDisplay(int number) {
 }
 
 void SingleDigitNumericDisplay::clearDisplay() {
-    this->ledA->write(1);
-    this->ledB->write(1);
-    this->ledC->write(1);
-    this->ledD->write(1);
-    this->ledE->write(1);
-    this->ledF->write(1);
-    this->ledG->write(1);
-    this->ledDP2->write(1);
+    ledA->write(1);
+    ledB->write(1);
+    ledC->write(1);
+    ledD->write(1);
+    ledE->write(1);
+    ledF->write(1);
+    ledG->write(1);
+    ledDP2->write(1);
 }
